79e8c354351890d6c645cffe52998b4a37fc8faa
AjayShukla007 <shuklaajay0838@gmail.com>
2024-03-29 22:47:17 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const ffmpeg = require('fluent-ffmpeg');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const ffmpeg = require("fluent-ffmpeg");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*',
-  },
+    origin: "*"
+  }
 });
 
 const rtspStreamUrl = process.env.RTPS_URL;
@@ -18,24 +18,24 @@ const rtspStreamUrl = process.env.RTPS_URL;
 // Function to start streaming
 const startStreaming = () => {
   const ffmpegCommand = ffmpeg(rtspStreamUrl)
-    .inputFormat('rtsp')
-    .outputFormat('mpegts')
-    .videoCodec('mpeg1video')
-    .audioCodec('mp2')
-    .size('640x480')
+    .inputFormat("rtsp")
+    .outputFormat("mpegts")
+    .videoCodec("mpeg1video")
+    .audioCodec("mp2")
+    .size("640x480")
     .videoBitrate(800)
     .audioBitrate(128)
     .inputFPS(30)
     .outputFPS(30)
-    .on('end', () => console.log('Streaming ended'))
-    .on('error', (err) => console.error('Error:', err))
-    .pipe(io.emit('stream')); // Emit to all connected clients
+    .on("end", () => console.log("Streaming ended"))
+    .on("error", err => console.error("Error:", err))
+    .pipe(io.emit("stream")); // Emit to all connected clients
 
   return ffmpegCommand;
 };
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
+io.on("connection", socket => {
+  console.log("Client connected");
 
   let ffmpegProcess;
 
@@ -44,18 +44,18 @@ io.on('connection', (socket) => {
     ffmpegProcess = startStreaming();
   }
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
 
     // Stop streaming when the last client disconnects
     if (io.engine.clientsCount === 0 && ffmpegProcess) {
-      ffmpegProcess.kill('SIGINT');
+      ffmpegProcess.kill("SIGINT");
       ffmpegProcess = null;
     }
   });
 });
 
-app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
